4d266066ab9683d6b930854c2d06ff912267868b
Majid <majidkouki3301@gmail.com>
2023-03-21 17:21:39 -0700

    Initial commit

---
+++ flyover.cu
@@ -0,0 +1,42 @@
+
+
+
+// // Draw diagonal lines across mesh
+// static _global_ void updateHeightmapKernel(float *heightMap,
+//                             unsigned int width, int timestep) {
+//     unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+//     unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+//     unsigned int i = y * width + x;
+
+//     heightMap[i] = (float)(((x + y + timestep) * 2) % 256) / 256.0f;
+// }
+
+
+
+// // Given x and y, copy a pixel from one map to another
+// float pixel;
+// if (y < shift) {
+//     unsigned int row = (mapHeight - shift) + y;
+//     pixel = *(map1 + x + (row * mapWidth));
+// } else {
+//     unsigned int row = y - shift;
+//     pixel = *(map0 + x + (row * mapWidth));
+// }
+
+
+
+// // Viewpoint control using CPU
+// if (autoFollow && (bottomRow != nullptr)) {
+//     float y = bottomRow[meshWidth / 2];
+//     targetTransY = anim_base_y - (y / 5.0f);
+// }
+
+
+
+// // Allocate GPU memory
+// cudaMalloc(&heightmap, numPoints * sizeof(float));
+
+
+
+// // Allocate CPU memroy
+// cudaMallocHost(&bottomRow, meshWidth * sizeof(float));
